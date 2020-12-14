0733cc823ada06a9257d1f31704f5be407ff969c
Mikhail Babaev <mmbabaev@gmail.com>
2020-12-15 02:13:34 +0300

    add table

--- Recycle/UI/TipsTable/TipsTableViewController.swift
+++ Recycle/UI/TipsTable/TipsTableViewController.swift
@@ -10,10 +10,40 @@ import UIKit
 
 class TipsTableViewController: UIViewController {
     
-    //@Inject var service: 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @Inject var service: TipService
+    
+    var tipCollections: [TipsCollection] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        service.loadTipsCollections { [weak self] result in
+            switch result {
+            case .success(let collections):
+                self?.tipCollections = collections
+                self?.tableView.reloadData()
+            case .failure(let error):
+                self?.show(error: error)
+            }
+        }
+    }
+}
+
+extension TipsTableViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TipsCell") as! TipsTableViewCell
+        
+        let tips = tipCollections[indexPath.row]
+        cell.configure(tips: tips)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tipCollections.count
     }
 }
